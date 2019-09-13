61ccf783b9632e79536b87621e54afc1eb01cedf
iyassr <yasserdo@DESKTOP-ROPQBST.localdomain>
2019-09-13 20:14:18 +0300

    add sensor_id & mac for post

--- application.py
+++ application.py
@@ -1,34 +1,40 @@
-#! python3 
+#! python3
 from flask import Flask, render_template, url_for, request, jsonify, redirect, flash
 from flask import make_response
+import pprint
 import time
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
-        file = open('data.txt','r', newline="\n")
+        file = open('data.txt', 'r', newline="\n")
         all_data = file.read()
-        print(all_data)
         file.close()
-        return all_data
+        return pprint.pformat(all_data)
     if request.method == 'POST':
-
-        file = open('data.txt','a+', newline="\n")
+        file = open('data.txt', 'a+', newline="\n")
         temp = request.form.get('temp')
-        file.write('\ntemp: {}, timestamp: {}'.format(temp,time.time()))
+        mac = request.form.get('mac')
+        timestamp = time.time()
+        htime = time.ctime(timestamp)
+        sensor_id = request.form.get('sensor_id')
+        json_data = {"mac": mac, "sensor_id": sensor_id,
+                     "temp": temp, "timestamp": int(timestamp), "time": htime}
+        file.write(str(json_data))
         file.close()
-        return ''
+        return 'posted'
+
 
 @app.route('/data/<int:temp>', methods=['GET'])
 def submit(temp):
     if request.method == 'GET':
-        file = open('data.txt','a+', newline="\n")
-        file.write('\ntemp: {}, timestamp: {}'.format(temp,time.time()))
+        file = open('data.txt', 'a+', newline="\n")
+        json_data = {"temp": temp, "timestamp": int(time.time())}
+        file.write(str(json_data))
         file.close()
-        return ''
-
-
+        return 'posted'
 
 
 if __name__ == '__main__':
--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import requests
 import time
 
-#requests.post('http://localhost:5000', data = {'temp':'1'})
+requests.post('http://localhost:5000', data = {'temp':'1','mac':'fsadfasdfasdf','sensor_id':'example_sensor_id'})
 
-requests.get('http://localhost:5000/data/99')
+#requests.get('http://localhost:5000/data/99')
