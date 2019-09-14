8ee582350c7425786ac857061259d000db28d0fa
iyassr <yasserdo@DESKTOP-ROPQBST.localdomain>
2019-09-14 11:51:29 +0300

    fix price

--- application.py
+++ application.py
@@ -4,10 +4,11 @@ from flask import make_response
 import pprint
 import time
 import random
+import json
 food_dict = {}
 food_dict = {'B4:21:8A:F0:13:44': {'type': 'orange', 'price': 8}}
 last_dict = {}
-price  = 25
+price = 25
 app = Flask(__name__)
 
 food_dict = {}
@@ -29,17 +30,20 @@ def main():
         return str(final_str)
     if request.method == 'POST':
         small_file = open('small_file.txt', 'w')
-        file = open('data.txt', 'a+', newline="\n")
-        temp = request.form.get('temp')
-        mac = request.form.get('mac')
-        timestamp = time.time()
-        htime = time.ctime(timestamp)
-        sensor_id = request.form.get('id')
-        #price = food_dict[mac]['price']
+        price_file = open('price_file.txt', 'w')
+        file=open('data.txt', 'a+', newline = "\n")
+        temp=request.form.get('temp')
+        mac=request.form.get('mac')
+        timestamp=time.time()
+        htime=time.ctime(timestamp)
+        sensor_id=request.form.get('id')
+        # price = food_dict[mac]['price']
         global price
-        price = price - (float(temp) * 0.005)
-        json_data = {'mac': mac, 'sensor_id': sensor_id,
+        price=price - (float(temp) * 0.005)
+        json_data={'mac': mac, 'sensor_id': sensor_id,
                      'temp': temp, 'timestamp': int(timestamp), 'time': htime, 'price': price}
+        price_file.write(str(price))
+        price_file.close()
         small_file.write(str(json_data))
         small_file.close()
         file.write(str(json_data))
@@ -54,15 +58,15 @@ def main():
         return 'posted'
 
 
-
 @app.route('/get_request', methods=['GET', 'POST'])
 def get_request():
     if request.method == 'GET':
-        small_file = open('small_file.txt', 'r').read()
-        return str(small_file)
+        small_file = open('small_file.txt', 'r')
+        data = small_file.read()
+        small_file.close()
+        return str(data)
     return ''
 
- 
 
 @app.route('/data/<int:temp>', methods=['GET'])
 def submit(temp):
@@ -88,14 +92,19 @@ def get_last_update():
 
 @app.route('/get_price', methods=['GET'])
 def get_price():
+    if request.method == 'GET':
+        price_file = open('price_file.txt', 'r')
+        data = price_file.read()
+        price_file.close()
+        return str(data)
+
     return last_price()
 
 
 def last_price():
     global food_dict
     global last_dict
-    #food_dict[last_dict['mac']]['price'] = food_dict[last_dict['mac']]['price'] - (float(last_dict['temp']) * 0.0005)
-
+    # food_dict[last_dict['mac']]['price'] = food_dict[last_dict['mac']]['price'] - (float(last_dict['temp']) * 0.0005)
     price_ = float("%0.2f" % food_dict[last_dict['mac']]['price'])
     return str(price_)
 
